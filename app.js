377f28372bd87db1c8e59d50d517da00943d9543
Lucy Follansbee <lfollansbee@gmail.com>
2016-09-09 10:59:01 -0600

    working api integration with client-side on basic search functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 var routes = require('./routes/index');
 
@@ -21,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    message: 'Very Nice!'
+  });
 });
+
+//SEARCH for recipes
+
+router.get('/search', function(req, res, next){
+  console.log(req.query);
+  var options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search',
+    // qs:{
+    //   query: 'bean dip',
+    //   type: 'appetizer',
+    //   cuisine: 'mexican',
+    //   number: '15'
+    // },
+    qs: req.query,
+    headers:{
+      'cache-control': 'no-cache',
+      'x-mashape-key': process.env.key
+    }
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    if(!error && response.statusCode == 200){
+      var parsed = JSON.parse(body);
+      res.json(parsed);
+    }
+  });
+})
+
+
 
 module.exports = router;
