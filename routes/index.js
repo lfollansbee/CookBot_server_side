28cdccd2bb292eb90803786c762d4902c9f58593
Lucy Follansbee <lfollansbee@gmail.com>
2016-09-13 16:44:09 -0600

    working fridge search to and from server

--- routes/index.js
+++ routes/index.js
@@ -52,5 +52,31 @@ router.get('/recipeId', function(req, res, next){
   });
 })
 
+router.get('/search/fridge', function(req, res, next){
+  var strings = JSON.stringify(req.query.query)
+  // console.log(strings);
+  var options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients',
+    qs: {
+      ingredients: strings,
+      number: 10
+    },
+    headers:{
+      'cache-control': 'no-cache',
+      'x-mashape-key': process.env.key
+    }
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    // console.log(body);
+    if(!error && response.statusCode == 200){
+      var parsed = JSON.parse(body);
+      res.json(parsed);
+    }
+  });
+})
+
+
 
 module.exports = router;
